d7b90567b919c8dde36c8a3723393acbca300426
Zel-hub7 <Zelalemyohannes75@gmail.com>
2023-10-09 11:12:41 +0300

    write some codes

---
+++ Person.rb
@@ -0,0 +1,33 @@
+class Person
+    attr_reader :id, :name, :age
+
+    def initialize(name = 'unknown', age = 0, parent_permission = true)
+        @id = generate_id
+        @name = name
+        @age = age
+        @parent_permission = parent_permission
+    end
+
+    def name = (new_name)
+        @name = new_name
+    end
+
+    def age=(new_age)
+        @age = new_age
+    end
+    
+    def can_use_services?
+        of_age? || @parent_permission
+    end
+
+    private
+
+    def of_age?
+      @age.to_i >= 18
+    end
+
+    def generate_id
+        rand(10000..99999)
+    end
+end
+
