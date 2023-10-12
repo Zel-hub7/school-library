cf974c5bfa8581ae556fed62906c1d747b62f18c
Zel-hub7 <Zelalemyohannes75@gmail.com>
2023-10-12 15:14:50 +0300

    change files

--- student.rb
+++ student.rb
@@ -1,11 +1,23 @@
 require_relative 'Person'
+require_relative 'classroom'
 
 class Student < Person
   attr_reader :classroom
 
   def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
     super(name: name, age: age, parent_permission: parent_permission)
+    assign_to_classroom(classroom) if classroom
+  end
+
+  def assign_to_classroom(classroom)
+    return if classroom == @classroom
+
+    if @classroom
+      @classroom.students.delete(self)
+    end
+
     @classroom = classroom
+    classroom.add_student(self)
   end
 
   def play_hooky
