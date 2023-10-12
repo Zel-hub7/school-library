fcd45a15ee0885f98c76cb57b16a4d57ef077d31
Zel-hub7 <Zelalemyohannes75@gmail.com>
2023-10-12 18:22:14 +0300

    modify files

--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 require_relative 'nameable'
 
 class Person < Nameable
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -37,7 +37,7 @@ class Person < Nameable
     @name
   end
 
-  def add_rental(rental)
-    @rentals << rental
+  def add_rental(book, date)
+    Rental.new(date, book, self)
   end
 end
